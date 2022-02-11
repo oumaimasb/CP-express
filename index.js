300d1096b11f28ce970885f3986863d364bcf4a4
oumaimasb <96146890+oumaimasb@users.noreply.github.com>
2022-02-11 20:58:32 +0100

    final

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require("express");
 const path = require("path");
-const members = require("./Members");
+var exphbs = require("express-handlebars");
+const { engine } = require("express-handlebars");
+
+// const members = require("./Members");
 const logger = require("./Middleware/logger");
 const app = express();
 
 //init middleware
-//app.use(logger);
+app.use(logger);
+
+//handlbars middleware
+app.engine(
+  "handlebars",
+  engine({ extname: ".handlebars", defaultLayout: "main" })
+);
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+//body parser middleware
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+//homepageroute
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 //static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/members", require("./Routes/API/members"));
+// app.use("/api/members", require("./Routes/API/members"));
 
 const PORT = process.env.PORT || 5000;
 
